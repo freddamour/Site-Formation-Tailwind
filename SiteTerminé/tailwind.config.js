e74dbb29f646b8026d1fbc65f392dd1843203655
Enzo USTARIZ <enzo.ustariz@hotmail.fr>
2021-01-06 14:16:59 +0100

    first commit

--- SiteTerminé/tailwind.config.js
+++ SiteTerminé/tailwind.config.js
@@ -1,20 +1,15 @@
 module.exports = {
-  purge:  ['./*.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      minHeight: {
-        0: "0",
-        // pour les cartes
-        500: "500px",
-        // pour l'accueil
-        650: "650px",
-        full: "100%",
-      },
+    theme: {
+        extend: {
+            minHeight: {
+                500: "500px",
+                650: "650px"
+            }
+        }
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+    variants: {
+        extend: {
+            borderWidth: ['hover']
+        }
+    }
+}
