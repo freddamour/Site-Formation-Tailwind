af8b05cfb1c6acd9d2a481083d139de2439b1316
frédéric <fdamour121@gmail.com>
2023-08-29 01:35:34 +0200

    Typographie

--- SiteTerminé/app.js
+++
@@ -1,8 +0,0 @@
-const menuResponsive = document.querySelector('.toggle-menu');
-const iconeToggle = document.querySelector('.icone-toggle');
-
-iconeToggle.addEventListener('click', () => {
-
-    menuResponsive.classList.toggle('hidden');
-
-})
